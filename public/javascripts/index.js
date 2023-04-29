b5fddf4171fe51ec061fb4781acde8c5ac3f39c1
alvin <ckuvn1854@gmail.com>
2023-04-29 14:19:07 +0800

    feat: add login router and hbs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,11 +3,3 @@ const searchCategories = document.querySelector("#search-categories");
 if (searchCategories.dataset.value) {
   searchCategories.value = searchCategories.dataset.value;
 }
-
-// console.log(searchCategories.options);
-// searchCategories.options.forEach((option) => {
-//   if (option.value === searchCategories.value) {
-//     return searchCategories.
-//   }
-// })
-// searchCategories.value = "中東料理";
