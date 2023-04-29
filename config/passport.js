f4b8407f858ed4c6a643e3065692bc5ecd3daa23
alvin <ckuvn1854@gmail.com>
2023-04-29 18:42:27 +0800

    feat: add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
+const FacebookStrategy = require("passport-facebook").Strategy;
 // 密碼雜湊
 const bcrypt = require("bcryptjs");
 
@@ -40,6 +40,37 @@ module.exports = (app) => {
               .catch((err) => done(err, false));
           })
           .catch((err) => done(err, false));
+      }
+    )
+  );
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ["email", "displayName"],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        const { name, email } = profile._json;
+        User.findOne({ email }).then((user) => {
+          // 如果有了就直接登入
+          if (user) return done(null, user);
+          const randomPassword = Math.random().toString(36).slice(-8);
+          bcrypt
+            .genSalt(10)
+            .then((salt) => bcrypt.hash(randomPassword, salt))
+            .then((hash) =>
+              User.create({
+                name,
+                email,
+                password: hash,
+              })
+            )
+            .then((user) => done(null, user))
+            .catch((err) => done(err, false));
+        });
       }
     )
   );
